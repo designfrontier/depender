026a8456612f5b61876752e98342eefb3e9b79b0
Daniel Sellers <daniel@designfrontier.net>
2019-10-07 08:06:44 -0600

    work part 2... added semver type checking and a base patch update

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const { exec, execSync } = require('child_process');
+const path = require('path');
 const ncu = require('npm-check-updates');
+const semverDiff = require('semver-diff');
+
+const config = require(path.join(process.cwd(), '.depender.config.json')); //TODO: make less brittle?
 
 ncu.run({
-    // Any command-line option can be specified here.
-    // These are set by default:
     jsonUpgraded: true,
     packageManager: 'npm', // check for yarn vs. npm
-    upgrade: true // TODO: maybe have this false and update manually
+    upgrade: false
 }).then((upgraded) => {
-    console.log('dependencies to upgrade:', upgraded);
+  const pack = require(path.join(process.cwd(), 'package.json'));
+  const deps = {...pack.dependencies, ...pack.devDependencies};
+
+  const upgrades = Object.keys(upgraded).map(k => {
+    const diff = semverDiff(deps[k].replace(/[^~]/, ''), upgraded[k].replace(/[^~]/, ''));
+    return {
+      dependency: k,
+      dev: typeof pack.devDependencies[k] !== 'undefined',
+      current: deps[k],
+      upgrade: upgraded[k],
+      type: diff
+    };
+  });
+
+  // group all the patch level upgrades
+  const groups = {
+    patch: upgrades.filter(i => i.type === 'patch'),
+    minor: upgrades.filter(i => i.type === 'minor'),
+    major: upgrades.filter(i => i.type === 'major')
+  };
+
+  groups.patch.forEach(d => {
+    const save = d.dev ? '--save-dev' : '--save';
+    execSync(`npm i ${save} ${d.dependency}@${d.upgrade}`); // TODO merge into a string not exec each one
+  });
+
+  // patch for each major upgrade
+  console.log('dependencies to upgrade:', config);
 });
